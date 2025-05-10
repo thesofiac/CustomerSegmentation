f3994e99be9de34d763d4a05a8073dbd3e8de267
thesofiac <sofia.rg.milo@gmail.com>
2025-05-10 16:37:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ elif menu == "Preveja a qual grupo um cliente pertence":
 
     if st.button("Prever"):
         input_df = pd.DataFrame([[int(f1), int(f2), dic_scholarity[f3], dic_marital[f4], float(f5), int(f6), int(f7), str(f8), int(f9), int(f10), int(f11), int(f12), int(f13), int(f14), int(f15), int(f16), int(f17), int(f18), int(f19), int(f20), int(dic_binary[f21]), int(dic_binary[f22]), int(dic_binary[f23]), int(dic_binary[f24]), int(dic_binary[f25]), int(dic_binary[f27]), int(f28), int(f29), int(dic_binary[f26])]], columns=['ID', 'Year_Birth', 'Education', 'Marital_Status', 'Income', 'Kidhome', 'Teenhome', 'Dt_Customer', 'Recency', 'MntWines', 'MntFruits', 'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts', 'MntGoldProds', 'NumDealsPurchases', 'NumWebPurchases', 'NumCatalogPurchases', 'NumStorePurchases', 'NumWebVisitsMonth', 'AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5', 'AcceptedCmp1', 'AcceptedCmp2', 'Complain', 'Z_CostContact', 'Z_Revenue', 'Response'])
-        st.dataframe(pd.DataFrame(sorted(lista), columns=['ID']))
+        st.dataframe(pd.DataFrame(input_df))
       
         #treated_input_df, ids = treat_columns(input_df, is_original=False)
         #input_df_scaled = scale_columns(treated_input_df)
