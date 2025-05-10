9077b588087824b0f1f9a3e6c3e94eca9ad8a8f1
thesofiac <sofia.rg.milo@gmail.com>
2025-05-10 20:38:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -274,12 +274,6 @@ if menu == "Entenda os dados":
       </h5>
     </div>
     """, unsafe_allow_html=True)
-  
-    st.markdown("""
-    <div style='text-align: justify'>
-    <h5>Distribuição e agrupamento dos dados antes do tratamento: <br><br></h5>
-    </div>
-    """, unsafe_allow_html=True)
 
     red_scale = [
     "#7f0000",
